ed468336b602255d61f9ca406e0b4d34d85e60a7
Jon Saad-Falcon <41205309+jonsaadfalcon@users.noreply.github.com>
2024-06-17 20:45:43 -0700

    update

--- fastchat/llm_judge/generate_mt_bench_candidates.py
+++ fastchat/llm_judge/generate_mt_bench_candidates.py
@@ -59,11 +59,31 @@ for model_name in models:
     print(f"Generating candidates for model: {model_name}")
     
     model_id = model_name.split("/")[1]
-    candidate_generation_command = f"python gen_model_answer.py --model-path {model_name} --model-id {model_id} --model-type TogetherAI"
+    candidate_generation_command = f"python gen_model_answer.py --model-path {model_name} --model-id {model_id} --model-type TogetherAI --num_choices {generation_dict['candidates_per_temp'][0]}"
 
     print("Generation Command: ", candidate_generation_command)
+    print("Generating candidates...")
+    #generation_result = subprocess.run(candidate_generation_command, shell=True, capture_output=True, text=True)
+    with subprocess.Popen(candidate_generation_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
+        for line in process.stdout:
+            print(line, end='')  # Print the output in real-time
 
-    result = subprocess.run(candidate_generation_command, shell=True, capture_output=True, text=True)
+    saved_jsonl_path = f"data/mt_bench/model_answer/{model_id}.jsonl"
 
-    print("Result: ", result)
+    ##########################################
+
+    judgement_command = f"python gen_judgment.py --model-list {model_name} --parallel 2"
+    #judgement_result = subprocess.run(judgement_command, shell=True, capture_output=True, text=True)
+    with subprocess.Popen(judgement_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
+        for line in process.stdout:
+            print(line, end='')  # Print the output in real-time
+
+    ##########################################
+
+    show_results_command = f"python show_result.py --model-list {model_name}"
+    #show_results_result = subprocess.run(show_results_command, shell=True, capture_output=True, text=True)
+    with subprocess.Popen(show_results_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
+        for line in process.stdout:
+            print(line, end='')  # Print the output in real-time
+
 
