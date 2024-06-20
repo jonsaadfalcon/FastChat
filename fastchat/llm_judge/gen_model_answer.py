9d739cd04b3e3f118888b631fb61f1c4412f23c0
Jon Saad-Falcon <41205309+jonsaadfalcon@users.noreply.github.com>
2024-06-20 12:47:09 -0700

    update

--- fastchat/llm_judge/gen_model_answer.py
+++ fastchat/llm_judge/gen_model_answer.py
@@ -183,7 +183,7 @@ def get_model_answers(
         
         model_id = model_path
     
-        if model == "microsoft/Phi-3-small-8k-instruct":
+        if model_id == "microsoft/Phi-3-small-8k-instruct":
             pipeline = transformers.pipeline(
                 "text-generation",
                 model=model_id,
